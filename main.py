0df8cd65c52409ad3d6394bb28d252c4d9754ae6
aliyan jabbar <jabbaraliyan805@gmail.com>
2025-03-13 21:07:53 +0500

    adding Password Strength Meter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import random
 import string
+import pyperclip
 
 
 def passwgen(length, use_int, use_special):
@@ -15,18 +16,32 @@ def passwgen(length, use_int, use_special):
     return "".join(random.choice(characters) for _ in range(length))
 
 
-# streamlit 
+# streamlit
 st.set_page_config(page_title="Password Generator", page_icon="🔒")
 st.title("🔒 Password Generator ")
 
 length = st.slider("Length of password", min_value=5, max_value=50, value=10)
-
 use_int = st.checkbox("Use Numbers 🔢")
-
 use_special = st.checkbox("Use Special Characters")
 
-if st.button("Generate Password 🧬"):
-    password = passwgen(length, use_int, use_special)
-    st.success(f"Generated Password: {password}")
+# Store password in session state
+if "password" not in st.session_state:
+    st.session_state.password = ""
 
+if st.button("Generate Password 🧬"):
+    st.session_state.password = passwgen(length, use_int, use_special)
+    st.success(f"Generated Password: {st.session_state.password}")
+
+# Separate copy button
+if st.button("Copy Generated Password"):
+    if st.session_state.password:
+        pyperclip.copy(st.session_state.password)
+        st.success("Password copied to clipboard! ✅")
+    else:
+        st.error("Please Generate a Password first ")
+
+if st.session_state.password:
+    st.markdown(
+        "[Check Strenght of your Generated Password 💪](https://aliyan-password-strength.streamlit.app/)"
+    )
 st.write("Made with ❤️ by Aliyan Jabbar")
